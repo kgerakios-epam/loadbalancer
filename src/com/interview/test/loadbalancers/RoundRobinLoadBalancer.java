f9a9a1f887da032372159dd801a5a5ce3a0680b1
kgerakios-epam <konstantinos_gerakios@epam.com>
2020-10-13 13:33:26 +0200

    Fix round robin counter to be atomic. Map negatives to non-negatives after atomic operation

--- src/com/interview/test/loadbalancers/RoundRobinLoadBalancer.java
+++ src/com/interview/test/loadbalancers/RoundRobinLoadBalancer.java
@@ -3,21 +3,32 @@ package com.interview.test.loadbalancers;
 import com.interview.test.providers.IProvider;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 final class RoundRobinLoadBalancer extends AbstractLoadBalancer implements LoadBalancer
 {
-    private volatile int counter = 0;
+    private AtomicInteger counter = new AtomicInteger(0);
 
     public RoundRobinLoadBalancer(final List<IProvider> providerList, final LoadBalancerConfiguration configuration)
     {
-        super(providerList,configuration);
+        super(providerList, configuration);
     }
 
     @Override
     protected int getNext()
     {
-        int i = counter%getSize();
-        counter = counter == Integer.MAX_VALUE? 0 : counter+1;
-        return i;
+        int i = counter.getAndIncrement();
+        if (i < 0)
+        {
+            if (i == Integer.MIN_VALUE)
+            {
+                i = 0;
+            }
+            else
+            {
+                i = Integer.MAX_VALUE + i + 1;
+            }
+        }
+        return i % getSize();
     }
 }
